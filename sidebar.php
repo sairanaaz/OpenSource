77a8c5018633da3a1b7f4995d7692d55a9371529
sairanaaz <12019020025@umt.edu.pk>
2014-06-14 23:00:30 +0500

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+		
+		<div id="sidebar">
+			<div class="sidebar-box">
+				<span class="sidebar-title">About Us</span>
+				<div class="dots"></div>			
+				<div class="textwidget">
+					<p>Project for the Subject Open source to create a worpdress theme</p>
+				</div>
+			</div>		
+			<div class="sidebar-box">		
+					<span class="sidebar-title">recent posts</span><div class="dots"></div>		
+				<ul>
+				<li><a href="http://sst.umt.edu.pk" title="Hello world!">Hello world!</a></li>
+				</ul>
+			</div>
+		</div><!-- sidebar -->		
+		<div class="spacer"></div>
+	</div><!-- wrapper -->
+	<div id="sidebar">
+            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("sidebar") ) : ?>
+            <?php endif; ?>
+        </div><!-- sidebar -->      
+        <div class="spacer"></div>
+    </div><!-- wrapper -->
+	</body>
+</html>
